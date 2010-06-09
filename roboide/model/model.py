7f536142c01d6ccbf24722481e1c19bd02c0c754
Sam Phippen <samphippen@googlemail.com>
2010-06-09 21:25:35 +0100

    move firmware models into it's own file

--- roboide/model/model.py
+++ roboide/model/model.py
@@ -53,66 +53,6 @@ class AutoSave(Entity):
     # The file contents
     content = Field(UnicodeText)
 
-class FirmwareTargets(Entity):
-    """Devices that we manage firmware for."""
-    # The name of the device.
-    name = Field(String(40))
-
-class FirmwareBlobs(Entity):
-    # The device (ForeignKey doesn't work in the sqlobject on button)
-    device = Field(Integer) #ForeignKey("FirmwareTargets")
-
-    # The version number
-    version = Field(Integer)
-
-    # The firmware filename
-    firmware = Field(String(40))
-
-    # The revision number in VC that the firmware is built from.
-    # Current supported formats:
-    #  - "svn:REV" where REV is the subversion commit number.
-    #  - "git:SHA" where SHA is the git commit hash.
-    revision = Field(String(40))
-
-    # A description of the firmware.  Could contain a changelog.
-    description = Field(UnicodeText)
-
-class FirmwareState(Entity):
-    # The firmware this relates to. (ForeignKey doesn't work in the sqlobject on button)
-    fw_id = Field(Integer) #ForeignKey("FirmwareBlobs")
-
-    # The date and time of state change
-    date = Field(Date)
-
-    # An message to go with the state change
-    message = Field(UnicodeText)
-
-    # The state the firmware changed to
-    state = Field(Enum([
-                                    "ALLOCATED",
-                                    "DEVEL",
-                                    "TESTING",
-                                    "SHIPPING",
-                                    "FAILED",
-                                    "OLD_RELEASE",
-                                    "SUPERCEDED"
-                       ]
-                , strict=True )
-            )
-
-class FirmwareTesting(Entity):
-    # The firmware this relates to. (ForeignKey doesn't work in the sqlobject on button)
-    fw_id = Field(Integer) #ForeignKey("FirmwareBlobs")
-
-    # The date and time of the test result
-    date = Field(Date)
-
-    # An message to go with the test result
-    message = Field(UnicodeText)
-
-    # The result of the firmware test
-    result = Field(Boolean)
-
 class UserBlogFeeds(Entity):
 	#the user id
 	user = Field(String(40))
