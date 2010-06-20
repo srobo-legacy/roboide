f6f8a5a8560d7a21de2727d12f95d53f6caa0842
Sam Phippen <samphippen@googlemail.com>
2010-06-20 23:20:13 +0100

    remove another debug print from a project helper

--- tests/helpers/projecthelpers.py
+++ tests/helpers/projecthelpers.py
@@ -8,5 +8,4 @@ from roboide.user import get_repopath
 def does_project_exist(team, project):
     repo_path = get_repopath(team).replace('file:///','/')
     project_path = repo_path + os.path.sep + project
-    print project_path
     return os.path.isdir(project_path)
