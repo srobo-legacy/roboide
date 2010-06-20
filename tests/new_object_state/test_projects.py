91bb826d65418a7680e2a99dbef2817d5c3a3582
Sam Phippen <samphippen@googlemail.com>
2010-06-20 23:18:45 +0100

    remove unnecessary debug print from project test

--- tests/new_object_state/test_projects.py
+++ tests/new_object_state/test_projects.py
@@ -39,4 +39,3 @@ class TestProjectFunctions(unittest.TestCase):
                 self.connection.request("GET", "/createproj?name=%s&team=%s" % (proj, team))
                 response = self.connection.getresponse()
                 self.assertEqual(helpers.does_project_exist(team, proj), True, "created project did not exist")
-
