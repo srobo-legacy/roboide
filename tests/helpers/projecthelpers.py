2764382ea3e21a8c2cfbdbb8542c22b9890e9a13
Sam Phippen <samphippen@googlemail.com>
2010-06-22 13:32:41 +0100

    refactor getting project path and repo path
    doing both of these things can now be done from within a helper function

--- tests/helpers/projecthelpers.py
+++ tests/helpers/projecthelpers.py
@@ -5,7 +5,12 @@ import os
 # roboide imports
 from roboide.user import get_repopath
 
+def get_repo_file_path(team):
+    return get_repopath(team).replace('file:///', '/')
+
+def get_project_path(team, project):
+    return get_repo_file_path(team) + os.path.sep + project
+
 def does_project_exist(team, project):
-    repo_path = get_repopath(team).replace('file:///','/')
-    project_path = repo_path + os.path.sep + project
+    project_path = get_project_path(team, project)
     return os.path.isdir(project_path)
