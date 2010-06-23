27ddc82bdfd7c0800014cbd3db984c12ba4735ae
Sam Phippen <samphippen@googlemail.com>
2010-06-23 18:23:51 +0100

    add helper to checkout repo

--- tests/helpers/projecthelpers.py
+++ tests/helpers/projecthelpers.py
@@ -1,6 +1,7 @@
 
 # Standard imports
 import os
+import subprocess
 
 # roboide imports
 from roboide.user import get_repopath
@@ -14,3 +15,6 @@ def get_project_path(team, project):
 def does_project_exist(team, project):
     project_path = get_project_path(team, project)
     return os.path.isdir(project_path)
+
+def checkout_repository(team, project, todir):
+    subprocess.check_call(["bzr", "checkout", get_project_path(team, project), todir], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
