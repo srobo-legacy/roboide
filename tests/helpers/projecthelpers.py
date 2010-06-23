14d07976fa275faced4f61d9b133576173f38580
Peter Law <PeterJCLaw@gmail.com>
2010-06-23 21:55:56 +0100

    Add docstrings to the test helper functions

--- tests/helpers/projecthelpers.py
+++ tests/helpers/projecthelpers.py
@@ -7,19 +7,34 @@ import subprocess
 from roboide.user import get_repopath
 
 def get_repo_file_path(team):
+    """
+    Return the path to a team's repo.
+    """
     return get_repopath(team).replace('file:///', '/')
 
 def get_project_path(team, project):
+    """
+    Return the full path to a project.
+    """
     return get_repo_file_path(team) + os.path.sep + project
 
 def does_project_exist(team, project):
+    """
+    Return whether or not a project exists in a team's repo.
+    """
     project_path = get_project_path(team, project)
     return os.path.isdir(project_path)
 
 def checkout_repository(team, project, todir):
+    """
+    Grab a temporary checkout of a team's repo.
+    """
     subprocess.check_call(["bzr", "checkout", get_project_path(team, project), todir], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 def file_exists_in_project(file, team, project):
+    """
+    Return whether or not a file exists in a project.
+    """
     checkout_path = "/tmp/repo"
     checkout_repository(team, project, checkout_path)
 
