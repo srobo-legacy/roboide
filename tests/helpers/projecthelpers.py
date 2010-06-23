e1de8a72196ad5d2ed6b375e81a3fc295ea2603c
Sam Phippen <samphippen@googlemail.com>
2010-06-23 18:26:42 +0100

    add helper to determine if a file exists in a repo
    checks out the repo, verifies if the file exists, and deletes the checkout

--- tests/helpers/projecthelpers.py
+++ tests/helpers/projecthelpers.py
@@ -18,3 +18,11 @@ def does_project_exist(team, project):
 
 def checkout_repository(team, project, todir):
     subprocess.check_call(["bzr", "checkout", get_project_path(team, project), todir], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def file_exists_in_project(file, team, project):
+    checkout_path = "/tmp/repo"
+    checkout_repository(team, project, checkout_path)
+
+    result = os.path.isfile(checkout_path + os.path.sep + file)
+    subprocess.call(["rm", "-rf", checkout_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return result
