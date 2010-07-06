29866260a8b88dd630e68166fd30c217c2d2ac40
Sam Phippen <samphippen@googlemail.com>
2010-07-06 16:52:17 +0100

    compare file contents in static files test

--- tests/dontcare_state/test_static.py
+++ tests/dontcare_state/test_static.py
@@ -42,6 +42,8 @@ class StaticFilesTest(unittest.TestCase):
                 self.connection.request("GET", file)
                 response = self.connection.getresponse()
                 self.assertEqual(response.status, 200, "response code on static file was not 200")
+                f = open("roboide" + os.path.sep + file)
+                self.assertEqual(f.read(), response.read(), "response file was not same as file on filesystem")
 
     def test_static_home_page(self):
         """
