4c777bfac91976acd70cc080c300fd8c8e57982e
Sam Phippen <samphippen@googlemail.com>
2010-07-07 22:19:40 +0100

    add test to verify loop syntax in the syntax check

---
+++ tests/resources/syntax/2.py
@@ -0,0 +1,17 @@
+from sr import *
+
+def main():
+    i = 3
+    while i < 17: i += 1
+
+    i = 0
+    while i < 34:
+        i += 17
+
+    for i in range(0, 14): print i
+
+    for i in range(0, 17):
+        print "ponies"
+
+    for pony in ["shire", "unicorn"]:
+        i -= 3
