ad7c946baabd186e8025dfdb0c2b5924825b551a
Sam Phippen <samphippen@googlemail.com>
2010-07-14 13:32:08 +0100

    remove debugging prints from confighelper

--- tests/helpers/confighelper.py
+++ tests/helpers/confighelper.py
@@ -1,5 +1,4 @@
 import ConfigParser
-import sys
 
 class ConfigHelper:
     """
@@ -8,14 +7,10 @@ class ConfigHelper:
     parser = ConfigParser.SafeConfigParser()
 
     def update_configuration(self, config_file_name):
-        print "parsertag", self.parser.read(config_file_name)
-        sys.stdout.flush()
+        self.parser.read(config_file_name)
 
     def get(self, configuration_item_name):
         section,key = configuration_item_name.split(".")
-        print section, key
-        print "sectionsflag", self.parser.sections()
-        sys.stdout.flush()
         return self.parser.get(section, key)
 
 global_config_helper = ConfigHelper()
