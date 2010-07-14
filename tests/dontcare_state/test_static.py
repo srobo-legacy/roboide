b704530d40f928732b525972e4570c6536f71a11
Sam Phippen <samphippen@googlemail.com>
2010-07-14 13:33:31 +0100

    make the static file test work

--- tests/dontcare_state/test_static.py
+++ tests/dontcare_state/test_static.py
@@ -38,10 +38,10 @@ class StaticFilesTest(unittest.TestCase):
 
         for file in self.files:
             if file.find(".") != -1:
-                self.connection.request("GET", file)
+                self.connection.request("GET", file.replace("/public", ""))
                 response = self.connection.getresponse()
                 self.assertEqual(response.status, 200, "response code on static file was not 200")
-                f = open("roboide" + os.path.sep + file)
+                f = open("roboide" + file)
                 self.assertEqual(f.read(), response.read(), "response file was not same as file on filesystem")
 
     def test_static_home_page(self):
